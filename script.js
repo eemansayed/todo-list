509705ee01aacb781b0ea35f6fb4d09f74c45592
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-31 11:36:26 +0000

    add function called addTodo

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ form.addEventListener("submit", (e) => {
 
   addTodo();
 });
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener("click", () =>
+      todoEl.classList.toggle("completed")
+    );
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+    });
+    todosUL.appendChild(todoEl);
+
+    input.value.value = "";
+  }
+}
