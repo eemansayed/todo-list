f76a2533c191de1d1b53724cacdac38d11475eff
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-31 11:27:05 +0000

    add event listner called form

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
